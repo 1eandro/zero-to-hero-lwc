f2154661b092a2a3708cee6a853662feaea76979
Leandro Fernandes <leandro.f.smart@gmail.com>
2023-08-13 20:25:38 +0200

    36. Understanding @track properties

--- force-app/main/default/lwc/data_binding/data_binding.js
+++ force-app/main/default/lwc/data_binding/data_binding.js
@@ -1,12 +1,42 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from "lwc";
 
 export default class Data_binding extends LightningElement {
-    fullname = "Zero to Hero"
-    title = "aura"
+  fullname = "Zero to Hero";
+  title = "aura";
 
-    changeHandler(event){
-        this.title = event.target.value
-    }
+  changeHandler(event) {
+    this.title = event.target.value;
+  }
 
+  /**
+   * Here we've seen that LWC tracks automatically only
+   * primitives.
+   * Track is used to track js objects and js arrays
+   *
+   * by 'track' understand that as a two-way data bindings
+   */
+  @track address = {
+    city: "Breda",
+    postcode: 3008,
+    country: "Netherlands",
+  };
 
+  trackHandler(event) {
+    this.address.city = event.target.value;
+  }
+
+  /**
+   * Another way to do that, also more advisable, is to pass
+   * a copy of the object to itself. Like this, you don't need to use the @track
+   */
+
+  spreadAddress = {
+    city: "Breda",
+    postcode: 7823,
+    country: "Netherlands",
+  };
+
+  trackHandlerWithSpread(event) {
+    this.spreadAddress = { ...this.address, city: event.target.value };
+  }
 }
