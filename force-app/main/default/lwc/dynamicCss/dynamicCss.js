c17fb41e9310ac81d36af6f1af10f15823f277f9
Leandro Fernandes <leandro.f.smart@gmail.com>
2023-08-19 14:57:22 +0200

    ...puttins some more cards and playing a bit with CSS

--- force-app/main/default/lwc/dynamicCss/dynamicCss.js
+++ force-app/main/default/lwc/dynamicCss/dynamicCss.js
@@ -1,13 +1,23 @@
 import { LightningElement } from "lwc";
 
 export default class DynamicCss extends LightningElement {
-  percent = 10;
+  percent = 0;
+  sliderValue;
 
   changeHandler(event) {
     this.percent = event.target.value;
   }
 
+  handleSlider(event) {
+    this.percent = event.detail.value;
+  }
+
   get percentage() {
     return `width: ${this.percent}%`;
+  }
+
+  get invertedPercentage() {
+    let invertedPercentage = 100 - this.percent;
+    return `width: ${invertedPercentage}%`;
   }
 }
