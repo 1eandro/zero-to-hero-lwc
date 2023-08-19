bcb852a8e3db4aea1d48509bdc5b28672f639a45
Leandro Fernandes <leandro.f.smart@gmail.com>
2023-08-19 18:03:46 +0200

    52. Lifecycle Hook - firing events of mounting phase

---
+++ force-app/main/default/lwc/lifeCycleParent/lifeCycleParent.js
@@ -0,0 +1,18 @@
+import { LightningElement } from "lwc";
+
+export default class LifeCycleParent extends LightningElement {
+  constructor() {
+    super();
+    console.log("Parent: Constructor Phase");
+  }
+
+  connectedCallback() {
+    console.log("Parent: connectedCallback Phase");
+  }
+
+  renderedCallback() {
+    console.log("Parent: renderedCallback Phase");
+    let someDiv = this.template.querySelector(".some-div");
+    console.log("Now I can find some div: ", someDiv);
+  }
+}
