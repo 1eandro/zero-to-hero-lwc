fabb8044039235cf9c2ec5a6f57eb61174db5540
Leandro Fernandes <leandro.f.smart@gmail.com>
2023-08-20 16:57:20 +0200

    59. Parent to Child, passing data - Non primitive data

--- force-app/main/default/lwc/p2cParentComponent/p2cParentComponent.js
+++ force-app/main/default/lwc/p2cParentComponent/p2cParentComponent.js
@@ -1,3 +1,26 @@
 import { LightningElement } from "lwc";
 
-export default class P2cParentComponent extends LightningElement {}
+export default class P2cParentComponent extends LightningElement {
+  carouselData = [
+    {
+      src: "https://picsum.photos/300",
+      header: "First",
+      description: "First description.",
+    },
+    {
+      src: "https://picsum.photos/301",
+      header: "Second",
+      description: "Second description.",
+    },
+    {
+      src: "https://picsum.photos/302",
+      header: "Third",
+      description: "Third description.",
+    },
+    {
+      src: "https://picsum.photos/303",
+      header: "Fourth",
+      description: "Fourth description.",
+    },
+  ];
+}
